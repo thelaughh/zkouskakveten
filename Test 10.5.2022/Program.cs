a9200c328e8536c26009861492496a437575c103
Jan Gergel <jan.gergel@educhem.cz>
2022-05-10 08:51:32 +0200

    Přidejte soubory projektu.

---
+++ Test 10.5.2022/Program.cs
@@ -0,0 +1,8 @@
+﻿using TestAlp;
+
+Test test = new Test();
+test.typtestu = "Algoritmizace";
+test.pocettestu = 16;
+test.pocetzaku = 24;
+test.vlastnosti();
+
---
+++ Test 10.5.2022/Test.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestAlp
+{
+    internal class Test
+    {
+        public string typtestu { get; set; }
+        public int pocettestu { get; set; }
+        public int pocetzaku { get; set; }
+
+        public Test()
+        {
+            Console.WriteLine();
+        }
+        public void vlastnosti()
+        {
+            Console.WriteLine($"{typtestu}, {pocettestu}, {pocetzaku}");
+        }
+
+    }
+}
